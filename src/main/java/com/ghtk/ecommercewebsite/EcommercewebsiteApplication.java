91eed12cccfc40cf5f313a9518feb7585457d818
Khanhdev <khanhquocphamdev@gmail.com>
2024-07-12 11:27:38 +0700

    first commit

---
+++ src/main/java/com/ghtk/ecommercewebsite/EcommercewebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ghtk.ecommercewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercewebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercewebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghtk/ecommercewebsite/EcommercewebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghtk.ecommercewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercewebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
