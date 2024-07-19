022cabae963b51b6a68d6292115245aea0bf22fa
Khanhdev <khanhquocphamdev@gmail.com>
2024-07-19 09:35:43 +0700

    push entity nothing

---
+++ src/main/java/com/ghtk/ecommercewebsite/entity/Address.java
@@ -0,0 +1,21 @@
+package com.ghtk.ecommercewebsite.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String country;
+    private String province;
+    private String district;
+    private String detail;
+    private String phone;
+    private String name;
+
+    // Getters and setters
+}
