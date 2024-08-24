c761dd7d9e4f1dbcdf5c13da4255862560f1bbd7
TruongLe26 <quangtruongpc88@gmail.com>
2024-08-24 13:38:38 +0700

    Refactor forgot password functionality

--- src/main/java/com/ghtk/ecommercewebsite/controllers/ForgotPasswordController.java
+++ src/main/java/com/ghtk/ecommercewebsite/controllers/ForgotPasswordController.java
@@ -1,11 +1,16 @@
 package com.ghtk.ecommercewebsite.controllers;
 
+import com.ghtk.ecommercewebsite.exceptions.DataNotFoundException;
+import com.ghtk.ecommercewebsite.models.dtos.request.EmailDTO;
 import com.ghtk.ecommercewebsite.models.responses.CommonResult;
 import com.ghtk.ecommercewebsite.services.OtpService;
+import com.ghtk.ecommercewebsite.services.user.UserService;
 import com.ghtk.ecommercewebsite.utils.ChangePassword;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/forgotPassword")
@@ -13,11 +18,22 @@ import org.springframework.web.bind.annotation.*;
 public class ForgotPasswordController {
 
     private final OtpService otpService;
+    private final UserService userService;
 
 //    @PostMapping("/verifyEmail/{email}")
 //    public ResponseEntity<String> verifyEmail(@PathVariable String email) {
 //        return otpService.verifyEmailAndSendOtp(email);
 //    }
+
+    @PostMapping("/verifyEmail")
+    public CommonResult<String> verifyEmailNewVersion(@RequestBody EmailDTO emailDTO) throws DataNotFoundException {
+        return CommonResult.success(userService.sendOtpForForgotPasswordRequest(emailDTO.getEmail()));
+    }
+
+    @PostMapping("/verifyOtp")
+    public CommonResult<String> verifyOtpNewVersion() {
+        return null;
+    }
 
     @PostMapping("/verifyEmail/{email}")
     public CommonResult<String> verifyEmail(@PathVariable String email) {
---
+++ src/main/java/com/ghtk/ecommercewebsite/models/dtos/request/EmailDTO.java
@@ -0,0 +1,14 @@
+package com.ghtk.ecommercewebsite.models.dtos.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailDTO {
+    private String email;
+}
